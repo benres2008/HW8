1d7b2d4f69a14f8c42dc49c3116935b937f6cb23
rez <benjaminresnicoff@icloud.com>
2020-10-15 18:57:01 -0500

    test

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// array of questions for user
-const questions = [
-
-];
+var fs = require("fs")
+var inquirer = require("inquirer")
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -9,7 +7,26 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
+    inquirer
+    .prompt([
+    {
+      type: "input",
+      message: "What is this app for?",
+      name: "App use"
+    },
+    {
+      type: "input",
+      message: "How do you use your app?",
+      name: "p"
+    },
+    {
+      type: "password",
+      message: "Re-enter password to confirm:",
+      name: "confirm"
+    }
+  ]).then(function(response) {
 
+    console.log(response)})
 }
 
 // function call to initialize program
